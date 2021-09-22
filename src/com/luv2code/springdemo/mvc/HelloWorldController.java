61c70d1dfa880b85f69f25a4f320b584ae405a1c
Adarsh Pathak <adarsh24699@gmail.com>
2021-09-22 16:09:05 +0530

    Binding request params by @RequestMapping in MVC

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -44,5 +45,24 @@ public class HelloWorldController {
 		return "helloworld";
 	}
 	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String theName,
+			Model model) {
+		/*
+		// read the request parameter from the HTML form.
+		String 	theName = request.getParameter("studentName");   // This commented code work can be done by @RequestParam
+		*/
+		
+		//convert the data to caps
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String 	result = "Hey my friend from V3! " + theName;
+		
+		//add message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
 
 }
